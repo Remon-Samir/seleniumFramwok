9e0c933d7892342b302476102d0aae3e51d98457
Remon Samir <119154108+Remon-Samir@users.noreply.github.com>
2025-03-15 13:10:42 +0200

    Update BingSearchResultPage.java
    fix null exception by removing webdriver from bingResearch Page

--- src/main/java/bing/BingSearchResultPage.java
+++ src/main/java/bing/BingSearchResultPage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 public class BingSearchResultPage extends Page {
 
     By thirdSearchResult = By.xpath("//h2/a[text()='TestNG Tutorial']");
-    WebDriver driver;
     public BingSearchResultPage(WebDriver driver){
         super(driver);
     }
